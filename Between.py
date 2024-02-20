3aa9c61bf3332bc405cfe66ce10b0fa55b4cc1b5
Krishna Vishwakarma <126193592+krishvish-0802@users.noreply.github.com>
2024-02-20 16:43:26 +0530

    Update Between.py

--- Between.py
+++ Between.py
@@ -2,11 +2,19 @@ def find_text_between_words(input_string, word1, word2):
     start_indices = [i + len(word1) for i in range(len(input_string)) if input_string.startswith(word1, i)]
     end_indices = [i for i in range(len(input_string)) if input_string.startswith(word2, i)]
 
+    last_end_index = -1
+
     for start_index in start_indices:
+        found = False
         for end_index in end_indices:
-            if start_index < end_index:
+            if end_index > last_end_index and start_index < end_index:
                 text_between = input_string[start_index:end_index]
                 print(f"Text between '{word1}' and '{word2}': {text_between}")
+                found = True
+                last_end_index = end_index
+                break
+        if found:
+            continue
 
 # Example usage
 input_string = "apple banana orange apple mango apple banana apple mango orange banana apple"
